e2541683e4a95fae9a13073f8fc88ee7af8be632
Ritesh Hon <honritesh@gmail.com>
2022-08-01 03:34:26 +0530

    responsive screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
+import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
+import 'package:instagram_flutter/responsive/web_screen_layout.dart';
+import 'package:instagram_flutter/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +14,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
       title: 'Instagram Clone',
-      home: Text("Let's Build Instagram"),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
+      ),
     );
   }
 }
---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const mobileBackgroundColor = Color(0xFF000000);
+const webBackgroundColor = Color(0xFF000000);
+const mobileSearchColor = Color(0xFF000000);
+const blueColor = Color(0xFF3498DB);
+const primaryColor = Colors.white;
+const secondaryColor = Colors.grey;
