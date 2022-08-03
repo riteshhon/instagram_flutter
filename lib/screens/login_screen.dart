81b7f088f294b742953e466740fc1390d47ef88b
Ritesh Hon <honritesh@gmail.com>
2022-08-04 01:59:11 +0530

    login screen ui completed

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_flutter/utils/colors.dart';
@@ -27,7 +29,7 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 40.0),
+          padding: EdgeInsets.symmetric(horizontal: 35.0),
           width: double.infinity,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -45,13 +47,71 @@ class _LoginScreenState extends State<LoginScreen> {
               const SizedBox(height: 65.0),
               // text field input for email
               TextFieldInput(
-                hintText: "Enter Email Address",
+                hintText: 'Enter Email Address',
                 textInputType: TextInputType.emailAddress,
                 textEditingController: _emailController,
               ),
+              const SizedBox(height: 25.0),
               // text field input for password
+              TextFieldInput(
+                hintText: 'Enter Password',
+                textInputType: TextInputType.text,
+                textEditingController: _passwordController,
+                isPass: true,
+              ),
+              const SizedBox(height: 25.0),
               // button login
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  child: const Text('Log In'),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(vertical: 12.0),
+                  decoration: const ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(4.0),
+                      ),
+                    ),
+                    color: blueColor,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15.0),
+              Flexible(
+                child: Container(),
+                flex: 2,
+              ),
               // transitioning to signing up
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    // ignore: sort_child_properties_last
+                    child: const Text("Don't have an account?"),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      // ignore: sort_child_properties_last
+                      child: const Text(
+                        'Create account',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 8,
+                        horizontal: 5,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
